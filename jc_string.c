7c6f7afea4ed4a421cd46329e74997229f290e71
Jody Bruchon <jody@jodybruchon.com>
2023-12-03 18:01:14 -0500

    Cosmetic changes

--- jc_string.c
+++ jc_string.c
@@ -19,15 +19,14 @@ extern int jc_strncaseeq(const char *s1, const char *s2, size_t len)
 			c1 = *(const unsigned char *)s1;
 			c2 = *(const unsigned char *)s2;
 			/* Transform upper case to lower case */
-			if (c1 == 0 || c2 == 0) return 1;
+			if (c1 == '\0' || c2 == '\0') return 1;
 			if (c1 >= 'A' && c1 <= 'Z') c1 |= 0x20;
 			if (c2 >= 'A' && c2 <= 'Z') c2 |= 0x20;
 			if (c1 != c2) return 1;
 		} else {
-			if (*s1 == 0) return 0;
+			if (*s1 == '\0') return 0;
 		}
-		s1++; s2++;
-		i++;
+		s1++; s2++; i++;
 	}
 	return 0;
 }
@@ -42,12 +41,12 @@ extern int jc_strcaseeq(const char *s1, const char *s2)
 			c1 = *(const unsigned char *)s1;
 			c2 = *(const unsigned char *)s2;
 			/* Transform upper case to lower case */
-			if (c1 == 0 || c2 == 0) return 1;
+			if (c1 == '\0' || c2 == '\0') return 1;
 			if (c1 >= 'A' && c1 <= 'Z') c1 |= 0x20;
 			if (c2 >= 'A' && c2 <= 'Z') c2 |= 0x20;
 			if (c1 != c2) return 1;
 		} else {
-			if (*s1 == 0) return 0;
+			if (*s1 == '\0') return 0;
 		}
 		s1++; s2++;
 	}
