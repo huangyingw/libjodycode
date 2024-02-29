306ba30e62464536cac70cc85df55cbaa2854fe6
Jody Bruchon <jody@jodybruchon.com>
2024-02-28 19:45:41 -0500

    jc_time: add unlikely() to error checks

--- jc_time.c
+++ jc_time.c
@@ -13,7 +13,6 @@
 #define NTTIME_CONSTANT 116444736000000000ULL;
 #define NTTIME_NSEC 10000000ULL;
 
-#define REQ_NUM(a) { if (a < '0' || a > '9') return JC_EDATETIME; }
 #define ATONUM(a,b) (a = b - '0')
 
 
@@ -21,9 +20,9 @@ static int twodigit_atoi(const char * restrict p)
 {
 	int i, val;
 
-	if (*p < '0' || *p > '9') return -1;
+	if (unlikely(*p < '0' || *p > '9')) return -1;
 	ATONUM(i, *p); val = i * 10; p++;
-	if (*p < '0' || *p > '9') return -1;
+	if (unlikely(*p < '0' || *p > '9')) return -1;
 	ATONUM(i, *p); val += i; p++;
 	return val;
 }
@@ -42,45 +41,47 @@ extern time_t jc_strtoepoch(const char * const datetime)
 	memset(&tm, 0, sizeof(struct tm));
 
 	/* Process year */
-	tm.tm_year = 1000;
-	REQ_NUM(*p); if (*p == '2') tm.tm_year = 2000; p++;
-	REQ_NUM(*p); ATONUM(i, *p); tm.tm_year += i * 100; p++;
-	i = twodigit_atoi(p); if (i < 1) goto error_datetime;
-	tm.tm_year = i - 1900;  /* struct tm year is since 1900 */
+	if (unlikely(*p < '0' || *p > '9')) goto error_datetime;
+	ATONUM(i, *p); tm.tm_year += i * 1000; p++;
+	if (unlikely(*p < '0' || *p > '9')) goto error_datetime;
+	ATONUM(i, *p); tm.tm_year += i * 100; p++;
+	i = twodigit_atoi(p); if (unlikely(i < 1)) goto error_datetime;
+	tm.tm_year += i;
+	tm.tm_year -= 1900;  /* struct tm year is since 1900 */
 	p += 2;
-	if (*p != '-') goto error_datetime;
+	if (unlikely(*p != '-')) goto error_datetime;
 	p++;
 	/* Process month (0-11, not 1-12) */
-	i = twodigit_atoi(p); if (i < 1) goto error_datetime;
+	i = twodigit_atoi(p); if (unlikely(i < 1)) goto error_datetime;
 	tm.tm_mon = i - 1;
 	p += 2;
-	if (*p != '-') goto error_datetime;
+	if (unlikely(*p != '-')) goto error_datetime;
 	p++;
 	/* Process day */
-	i = twodigit_atoi(p); if (i < 1) goto error_datetime;
+	i = twodigit_atoi(p); if (unlikely(i < 1)) goto error_datetime;
 	tm.tm_mday = i;
 	p += 2;
 	/* If YYYY-MM-DD is specified only, skip the time part */
 	if (*p == '\0') goto skip_time;
-	if (*p != ' ') goto error_datetime; else p++;
+	if (unlikely(*p != ' ')) goto error_datetime; else p++;
 	/* Process hours */
-	i = twodigit_atoi(p); if (i < 1) goto error_datetime;
+	i = twodigit_atoi(p); if (unlikely(i < 1)) goto error_datetime;
 	tm.tm_hour = i;
 	p += 2;
-	if (*p != ':') goto error_datetime;
+	if (unlikely(*p != ':')) goto error_datetime;
 	p++;
 	/* Process minutes */
-	i = twodigit_atoi(p); if (i < 1) goto error_datetime;
+	i = twodigit_atoi(p); if (unlikely(i < 1)) goto error_datetime;
 	tm.tm_min = i;
 	p += 2;
-	if (*p != ':') goto error_datetime;
+	if (unlikely(*p != ':')) goto error_datetime;
 	p++;
 	/* Process seconds */
-	i = twodigit_atoi(p); if (i < 1) goto error_datetime;
+	i = twodigit_atoi(p); if (unlikely(i < 1)) goto error_datetime;
 	tm.tm_sec = i;
 	p += 2;
 	/* Junk after datetime string should cause an error */
-	if (*p != '\0') goto error_datetime;
+	if (unlikely(*p != '\0')) goto error_datetime;
 skip_time:
 	tm.tm_isdst = -1;  /* Let the host library decide if DST is in effect */
 	errno = 0;
